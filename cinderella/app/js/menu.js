8177565d9b86d024c175a429975dac26ee2555a1
IlyaStip <sii98@list.ru>
2021-06-27 14:46:07 +0300

    remove debugger

--- cinderella/app/js/menu.js
+++ cinderella/app/js/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var btns = $(".mobileMeu li a");
   btns.removeClass('active');
-  debugger;
+
   for (var i = 0; i < btns.length; i++) {
     console.log(btns[i]);
     if (window.location.href.indexOf(btns[i].href) > -1) {
